cfa216e21356e7909298f4ff9598df62313c1e24
LaoBan-ywcm <2459007415@qq.com>
2017-07-17 14:09:42 +0800

    游戏的基本逻辑完成

--- src/index.js
+++ src/index.js
@@ -7,23 +7,76 @@ import './index.css';
 
 
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
+function Square(props){
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
+
+function caculateWinner(squares){
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for(let i=0;i<lines.length;i++){
+        let [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+            return squares[a];
+        }
     }
+    return false;
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />;
+
+    constructor(props){
+        super(props);
+        this.state = {
+            squares:Array(9).fill(null),
+            xIsNext:true
+        }
     }
 
+    handleClick(i){
+        let squares = this.state.squares.slice();
+        if(caculateWinner(this.state.squares) || squares[i]){
+            return ;
+        }
+        squares[i] = this.state.xIsNext? 'X' : 'O';
+        this.setState({
+            squares:squares,
+            xIsNext:!this.state.xIsNext
+        })
+    }
+
+    renderSquare(i) {
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => {this.handleClick(i)}}
+        />;
+    }
+
+
+
     render() {
-        const status = 'Next player: X';
+        const winner = caculateWinner(this.state.squares);
+
+        let status;
+
+        if(winner){
+            status = 'Winner is ' + winner;
+        }else{
+            status = 'Next player:' + (this.state.xIsNext? 'X' : 'O');
+        }
 
         return (
             <div>
